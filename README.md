c6c0f7b45fa1f1c1f700927d0bc2d7fa0a50cef1
zhangxj <xiaojunzhang11@creditease.cn>
2018-11-21 15:48:53 +0800

    ..

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # tornadoweb
+
+## How to install tornadoweb
+```
+pip install tornadoweb
+```
+
+## How to create tornado project
+
+```
+tornadoweb_init project
+```
+
+## How to run web application
+```
+cd project
+python apprun.py
+```
+## How to run web application with options
+```
+cd project
+python apprun.py -h
+
+Usage: run.py [options]
+
+Options:
+  -h, --help       show this help message and exit
+  --port=PORT      Listen port.
+  --config=CONFIG  config
+
+python apprun.py --port=80 --config=settings.py
+```
+
+## How to use buildins settings
+```
+USE __conf__.PORT ...
+```
+
--- setup.py
+++ setup.py
@@ -7,17 +7,24 @@ long_description = ""
 
 setup(
     name="tornadoweb",
-    version="0.0.6",
+    version="0.0.11",
     author="9wfox",
     author_email="568628130@qq.com",
     description="tornado web",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/9wfox/tornadoweb",
+    package_data={'':['*.*']},
     packages=["tornadoweb"],
+    install_requires=["tornado"],
     classifiers=[
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points='''
+	[console_scripts]
+	tornadoweb_init=tornadoweb.tornadoweb_init:go
+    ''',
 )
